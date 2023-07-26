0d5b2cbd4b533a8f63e2368c182e3eb2857f21d1
ratovia <ratovia@hotmail.co.jp>
2023-07-26 14:16:51 +0900

    lockする

--- recording_server/server.py
+++ recording_server/server.py
@@ -11,27 +11,32 @@ cap = cv2.VideoCapture(0) # Capture video from default camera
 
 logging.basicConfig(level=logging.DEBUG)  # Setup logging
 
+lock = threading.Lock()  # Add this line at the beginning of your code
+
 @app.route('/record', methods=['POST'])
 def record():
     global recording
     global recorder
     global cap
+    global lock  # Add this line
 
     action = request.form.get('action')
     logging.debug(f'Received action: {action}')  # Log the received action
 
     if action == 'start':
-        if not recording: # Start recording if not already recording
-            logging.debug('Starting recording.')  # Log recording start
-            fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Define the codec
-            recorder = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640,480))  # Define VideoWriter
-            recording = True
+        with lock:  # Add this line
+            if not recording: # Start recording if not already recording
+                logging.debug('Starting recording.')  # Log recording start
+                fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Define the codec
+                recorder = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640,480))  # Define VideoWriter
+                recording = True
     elif action == 'stop':
-        if recording: # Stop recording if currently recording
-            logging.debug('Stopping recording.')  # Log recording stop
-            recording = False
-            recorder.release() # Release the VideoWriter
-            recorder = None
+        with lock:  # Add this line
+            if recording: # Stop recording if currently recording
+                logging.debug('Stopping recording.')  # Log recording stop
+                recording = False
+                recorder.release() # Release the VideoWriter
+                recorder = None
 
     return '', 200
 
@@ -39,14 +44,15 @@ def capture_video():
     global recording
     global recorder
     global cap
+    global lock  # Add this line
 
     while True:
         ret, frame = cap.read() # Capture a frame from the camera
 
-        if recording and ret:
-            logging.debug('Recording frame.')  # Log frame recording
-            recorder.write(frame)  # Write the frame to the file if recording
-
+        with lock:  # Add this line
+            if recording and ret:
+                logging.debug('Recording frame.')  # Log frame recording
+                recorder.write(frame)  # Write the frame to the file if recording
 if __name__ == '__main__':
     video_thread = threading.Thread(target=capture_video) # Create a thread that captures video
     logging.debug('Starting video capture thread.')  # Log thread start
