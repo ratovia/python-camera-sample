f3478415f7e5b496bdf9b98c256485290443f462
ratovia <ratovia@hotmail.co.jp>
2023-07-26 15:56:20 +0900

    streaming_server

---
+++ streaming_server/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, Response
+import threading
+
+from streaming_server.VideoCamera import VideoCamera
+
+app = Flask(__name__)
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def generate(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+if __name__ == '__main__':
+    threading.Thread(target=app.run, kwargs={'host':'0.0.0.0','port':5001}).start()
