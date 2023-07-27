e7db91b15e8c58c692ddc604bcd7dc7ac758f6e2
ratovia <ratovia@hotmail.co.jp>
2023-07-27 17:26:23 +0900

    修正

--- recording_server/server.py
+++ recording_server/server.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import threading
 import cv2
 import logging
+import atexit
 
 app = Flask(__name__)
 
@@ -23,15 +24,14 @@ def record():
     action = request.form.get('action')
     logging.debug(f'Received action: {action}')  # Log the received action
 
-    if action == 'start':
-        with lock:  # Add this line
+    with lock:  # Wrap the entire conditional in lock
+        if action == 'start':
             if not recording: # Start recording if not already recording
                 logging.debug('Starting recording.')  # Log recording start
                 fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Define the codec
                 recorder = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640,480))  # Define VideoWriter
                 recording = True
-    elif action == 'stop':
-        with lock:  # Add this line
+        elif action == 'stop':
             if recording: # Stop recording if currently recording
                 logging.debug('Stopping recording.')  # Log recording stop
                 recording = False
@@ -53,6 +53,25 @@ def capture_video():
             if recording and ret:
                 logging.debug('Recording frame.')  # Log frame recording
                 recorder.write(frame)  # Write the frame to the file if recording
+
+@atexit.register
+def cleanup():
+    global recording
+    global recorder
+    global cap
+    global lock  # Add this line
+
+    with lock:
+        if recording:  # Check if still recording
+            logging.debug('Force stopping recording.')  # Log force stopping
+            recording = False
+            if recorder:
+                recorder.release()  # Release the VideoWriter
+                recorder = None
+
+        if cap:
+            cap.release()  # Release the VideoCapture
+
 if __name__ == '__main__':
     video_thread = threading.Thread(target=capture_video) # Create a thread that captures video
     logging.debug('Starting video capture thread.')  # Log thread start
